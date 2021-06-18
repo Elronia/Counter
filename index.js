8633669a3fc8e2bdac288b2a9ac4c690577c77e4
Elronia <zar.catherine@gmail.com>
2021-06-18 00:04:00 -0400

    setting the count into o

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // document.getElementById("count-el").innerText = 5
+let count = 0
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
-let count = 0
+
 
 function increment() {
     count += 1
@@ -11,4 +12,9 @@ function increment() {
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
 }
+
+console.log("Let's count people on the subway!")
+
